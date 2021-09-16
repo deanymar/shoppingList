3cd99aa8119456b429c77968beb7aeea1da00ad7
deanymar <dean.y.marko@gmail.com>
2021-09-16 15:15:28 +0300

    master

--- shopping.py
+++ shopping.py
@@ -5,42 +5,35 @@ root.title("Shopping List")
 list = []
 
 # ------buttons Function------
+
+
 def btnAdd():
     list.append(item.get())
-    item.delete(0,END)
+    item.delete(0, END)
+
+
 def btnShow():
     for item in list:
-        myListBox.insert(END,item)
-    
+        if item not in myListBox.get(0,END):
+            myListBox.insert(END,item)
 # ------buttons function------
 
+
 # ------buttons------
-addListbtn = Button(root,text= "Add To List",command=btnAdd)
-addListbtn.grid(row = 3,column= 2)
-show = Button(root,text= "Show List",command=btnShow)
-show.grid(row =4 ,column=2)
+addListbtn = Button(root, text="Add To List", command=btnAdd)
+addListbtn.grid(row=3, column=2)
+show = Button(root, text="Show List", command =btnShow)
+show.grid(row=4, column=2)
 # ------buttons------
 
-myListBox = Listbox(root,width=20)
-myListBox.grid(row=6,column=2)
+myListBox = Listbox(root, width=20)
+myListBox.grid(row=6, column=2)
 
-lbl = Label(root,text= "Item")
-lbl.grid(row =1,column =1)
+lbl = Label(root, text="Item")
+lbl.grid(row=1, column=1)
 
-item = Entry(root,width = 20)
-item.grid(row = 1, column =2)
-
-
-
-
-
-
-
-
-
-
-
-
+item = Entry(root, width=20)
+item.grid(row=1, column=2)
 
 
 root.mainloop()
